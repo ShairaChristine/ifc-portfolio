b84ecbf220d32b2b281ed9a32e796390962400e0
shairachristine <shaics03@gmail.com>
2024-10-23 05:03:30 +0800

    New page > gradient moving bg

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/new', function () {
+    return view('new');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
